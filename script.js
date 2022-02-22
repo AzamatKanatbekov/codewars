ebbedc2e0215296674a286633c346f8cd8d93ca5
Azamat Kanatbekov <azamat.kanatbekov.kk@gmail.com>
2022-02-22 14:56:42 +0600

    practice day 4

--- script.js
+++ script.js
@@ -140,6 +140,22 @@
 // const cannonsReady = gunners => Object.values(gunners).every(el => el === 'aye') ? 'Fire!' : 'Shiver me timbers!'
 // console.log(cannonsReady({'aye':'aye','Joe':'aye','Johnson':'aye','Peter':'aye'}))
 
+// For Twins: 2. Math operations
+
+// Task:
+// A magician in the subway showed you a trick, he put an ice brick in a bottle to impress you.
+// The brick's length and width are equal, forming a square; its height may be different. Just for fun and also to impress the magician and people around, you decided to calculate the brick's volume. Write a function iceBrickVolume that will accept these parameters:
+//
+// radius - bottle's radius (always > 0);
+// bottleLength - total bottle length (always > 0);
+// rimLength - length from bottle top to brick (always < bottleLength);
+// And return volume of ice brick that magician managed to put into a bottle.
+
+// const iceBrickVolume = (radius, bottleLength, rimLength) => {
+//   return (bottleLength - rimLength) * radius * 2 * radius;
+// }
+
+// console.log(iceBrickVolume(1,10,2))
 
 
 
