624552b44eb390605beced94cf5ce6fadf373618
Azamat Kanatbekov <azamat.kanatbekov.kk@gmail.com>
2022-03-02 13:00:27 +0600

    practice day 7

--- script.js
+++ script.js
@@ -251,6 +251,7 @@ Complete the solution so that it reverses the string passed into it.
 const solution = (str) => str.split('').reverse().join('')
 
 console.log(solution('world'))
+console.log(solution('world'))
 
 
 
