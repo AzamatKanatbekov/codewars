f9f09d73eedccc0d0b5b1f7ff6e2880adeba0e03
Azamat Kanatbekov <azamat.kanatbekov.kk@gmail.com>
2022-03-02 12:35:07 +0600

    practice day 7

--- script.js
+++ script.js
@@ -229,12 +229,17 @@
 //
 // console.log(findNeedle(['hay', 'junk', 'hay', 'moreJunk', 'needle', 'randomJunk']))
 
+// Reversed sequence
 
+// Build a function that returns an array of integers from n to 1 where n>0.
+//
+// Example : n=5 --> [5,4,3,2,1]
 
+// Array менен массивке айлантып алабыз
+// Метод fill() заполняет все элементы массива от начального до конечного индексов одним значением
+const reverseSeq = n =>  Array(n).fill(0).map((e, i) => n - i )
 
-
-
-
+console.log(reverseSeq(5))
 
 
 
