760e9eab245725237695ed12f25ae83770868e59
Azamat Kanatbekov <azamat.kanatbekov.kk@gmail.com>
2022-03-17 18:26:53 +0600

    practice day 20 codewars

--- codewars.js
+++ codewars.js
@@ -687,9 +687,12 @@ console.log(lovefunc(897,993))
 
 
 
+Enumerable Magic #25 - Take the First N Elements
 
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
 
-
+const take = (array, n) => array.slice(0, n)
+console.log(take([0, 1, 2, 3, 5, 8, 13], 5))
 
 
 
