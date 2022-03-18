6ff81c2c126ccc4bb394c844b973304570dad009
Azamat Kanatbekov <azamat.kanatbekov.kk@gmail.com>
2022-03-18 21:42:28 +0600

    practice day 21 codewars

--- codewars.js
+++ codewars.js
@@ -698,15 +698,22 @@
 // Hex to Decimal
 // Complete the function which converts hex number (given as a string) to a decimal number.
 // Функция parseInt() принимает строку в качестве аргумента и возвращает целое число в соответствии с указанным основанием системы счисления.
-const hexToDec = hexString => parseInt(hexString, 16)
-console.log(hexToDec('FF'))
+// const hexToDec = hexString => parseInt(hexString, 16)
+// console.log(hexToDec('FF'))
+
+
+// Is it even?
+// In this Kata we are passing a number (n) into a function.
 //
+// Your code will determine if the number passed is even (or not).
 //
+// The function needs to return either a true or false.
 //
+// Numbers may be positive or negative, integers or floats.
 //
-//
-//
-//
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+const testEven = n => n % 2 === 0 ? true : false
+console.log(testEven(1))
 //
 //
 //
